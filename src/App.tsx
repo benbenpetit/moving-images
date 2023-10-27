817cbe802cd94430977f5cab1cf7755e038be9eb
Benoit Petit <petitbenoit3@gmail.com>
2023-10-27 17:56:12 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <>
+      <header>
+        <a href='#'>CONFIOTE</a>
+      </header>
+      <main>
+        <h1>
+          <span>JUSTE</span>
+          <span>UNE</span>
+          <span>CONFIOTE</span>
+          <span>FIGUE-NOIX</span>
+        </h1>
+      </main>
+    </>
+  )
+}
+
+export default App
