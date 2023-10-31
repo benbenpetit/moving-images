7aecbcacb220e935dc83abd3c160ee6c88e2292c
Benoit Petit <petitbenoit3@gmail.com>
2023-10-31 15:29:36 +0100

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,128 @@
 import gsap from 'gsap'
-import { useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import { throttle } from 'throttle-debounce'
 
 const getImageUrl = (i: number) => {
-	return new URL(`/src/assets/img/${i}.jpeg`, import.meta.url).href
+  return new URL(`/src/assets/img/${i}.jpeg`, import.meta.url).href
 }
 
+const lerp = (a: number, b: number, n: number) => (1 - n) * a + n * b
+
 const App = () => {
-	const imagesContainerRef = useRef<HTMLDivElement>(null)
+  const requestRef = useRef<number>(0)
+  const imagesContainerRef = useRef<HTMLDivElement>(null)
 
-	useEffect(() => {
-		const imagesContainer = imagesContainerRef.current!
-		let i = 0
-		let timestamp = 0
-		let lastMouseX = 0
-		let lastMouseY = 0
-		let movement = { x: 0, y: 0 }
-		let lastImagePosition = { x: 0, y: 0 }
+  const cacheMousePos = useRef({ x: 0, y: 0 })
+  const mousePos = useRef({ x: 0, y: 0 })
+  const lastImagePosition = useRef({ x: 0, y: 0 })
 
-		const handleMouseSpeed = (e: MouseEvent) => {
-			if (timestamp === null) {
-				timestamp = Date.now()
-				lastMouseX = e.screenX
-				lastMouseY = e.screenY
-				return
-			}
+  const setCachedMousePosition = useCallback(() => {
+    cacheMousePos.current.x = lerp(
+      cacheMousePos.current.x || mousePos.current.x,
+      mousePos.current.x,
+      0.1
+    )
+    cacheMousePos.current.y = lerp(
+      cacheMousePos.current.y || mousePos.current.y,
+      mousePos.current.y,
+      0.1
+    )
+    requestRef.current = requestAnimationFrame(setCachedMousePosition)
+  }, [requestRef])
 
-			const now = Date.now()
-			const dt = now - timestamp
-			const dx = e.screenX - lastMouseX
-			const dy = e.screenY - lastMouseY
-			const speedX = dx / dt
-			const speedY = dy / dt
-			movement = { x: speedX, y: speedY }
+  useEffect(() => {
+    const imagesContainer = imagesContainerRef.current!
+    let i = 0
 
-			timestamp = now
-			lastMouseX = e.screenX
-			lastMouseY = e.screenY
-		}
+    const addImage = (x: number, y: number) => {
+      const imgSrc = getImageUrl(i % 8)
+      const img = new Image()
+      img.src = imgSrc
+      img.onload = () => {
+        const image = document.createElement('img')
+        image.src = imgSrc
+        imagesContainer.appendChild(image)
+        gsap.fromTo(
+          image,
+          {
+            x: cacheMousePos.current.x,
+            y: cacheMousePos.current.y
+          },
+          {
+            duration: 2,
+            x,
+            y,
+            ease: 'power4',
+            onStart: () => {
+              if (imagesContainer.contains(image)) {
+                gsap.to(image, {
+                  duration: 0.5,
+                  opacity: 0,
+                  delay: 1,
+                  ease: 'power4.out',
+                  onComplete: () => {
+                    if (imagesContainer.contains(image)) {
+                      imagesContainer.removeChild(image)
+                    }
+                  }
+                })
+              }
+            }
+          }
+        )
+      }
+    }
 
-		const addImage = (x: number, y: number) => {
-			x = x - 100
-			y = y - 150
-			const imgSrc = getImageUrl(i % 8)
-			const img = new Image()
-			img.src = imgSrc
-			img.onload = () => {
-				const image = document.createElement('img')
-				image.src = imgSrc
-				imagesContainer.appendChild(image)
-				gsap.fromTo(
-					image,
-					{
-						x,
-						y
-					},
-					{
-						duration: 2,
-						x: x + movement.x * 16,
-						y: y + movement.y * 16,
-						ease: 'power4',
-						onStart: () => {
-							if (imagesContainer.contains(image)) {
-								gsap.to(image, {
-									duration: 0.5,
-									opacity: 0,
-									delay: 1,
-									ease: 'power4.out',
-									onComplete: () => {
-										if (imagesContainer.contains(image)) {
-											imagesContainer.removeChild(image)
-										}
-									}
-								})
-							}
-						}
-					}
-				)
-			}
-		}
+    const handleMouseMove = () => {
+      if (
+        mousePos.current.x > lastImagePosition.current.x + 80 ||
+        mousePos.current.x < lastImagePosition.current.x - 80 ||
+        mousePos.current.y > lastImagePosition.current.y + 80 ||
+        mousePos.current.y < lastImagePosition.current.y - 80
+      ) {
+        addImage(mousePos.current.x, mousePos.current.y)
+        i++
+        lastImagePosition.current = {
+          x: mousePos.current.x,
+          y: mousePos.current.y
+        }
+        if (imagesContainer.children.length > 12) {
+          imagesContainer.removeChild(imagesContainer.firstChild!)
+        }
+      }
+    }
 
-		const handleMouseMove = (e: MouseEvent) => {
-			const { clientX: x, clientY: y } = e
+    const throttleHandleMouseMove = throttle(10, handleMouseMove)
 
-			if (
-				x > lastImagePosition.x + 80 ||
-				x < lastImagePosition.x - 80 ||
-				y > lastImagePosition.y + 80 ||
-				y < lastImagePosition.y - 80
-			) {
-				addImage(x, y)
-				i++
-				lastImagePosition = { x, y }
-				if (imagesContainer.children.length > 12) {
-					imagesContainer.removeChild(imagesContainer.firstChild!)
-				}
-			}
-		}
+    document.addEventListener('mousemove', (e) => {
+      throttleHandleMouseMove()
+      mousePos.current = { x: e.clientX - 100, y: e.clientY - 150 }
+    })
 
-		const throttleHandleMouseMove = throttle(10, handleMouseMove)
+    requestRef.current = requestAnimationFrame(setCachedMousePosition)
 
-		document.addEventListener('mousemove', (e) => {
-			throttleHandleMouseMove(e)
-			handleMouseSpeed(e)
-		})
+    return () => {
+      cancelAnimationFrame(requestRef.current)
+      document.removeEventListener('mousemove', throttleHandleMouseMove)
+    }
+  }, [setCachedMousePosition])
 
-		return () => {
-			document.removeEventListener('mousemove', throttleHandleMouseMove)
-		}
-	})
-
-	return (
-		<>
-			<header>
-				<a href='#'>CONFIOTE</a>
-			</header>
-			<main>
-				<h1>
-					<span>JUSTE</span>
-					<span>UNE</span>
-					<span>CONFIOTE</span>
-					<span>FIGUE-NOIX</span>
-				</h1>
-				<div ref={imagesContainerRef} className='images-container'></div>
-			</main>
-		</>
-	)
+  return (
+    <>
+      <header>
+        <a href='#'>CONFIOTE</a>
+      </header>
+      <main>
+        <h1>
+          <span>JUSTE</span>
+          <span>UNE</span>
+          <span>CONFIOTE</span>
+          <span>FIGUE-NOIX</span>
+        </h1>
+        <div ref={imagesContainerRef} className='images-container'></div>
+      </main>
+    </>
+  )
 }
 
 export default App
